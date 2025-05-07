405b4eea9fe72f9d77e0027ab807a3e529eebbfa
RobertoHolanda81 <holandaroberto81@gmail.com>
2025-05-07 21:12:58 +0900

    "segundo commit"

---
+++ modulos.py
@@ -0,0 +1,50 @@
+import math  #importando o módulo math
+
+print(math.sqrt(81)) #imprimindo a raiz quadrada de 81
+
+print(math.pi) #imprimindo o valor de pi 
+
+print(math.factorial(5)) #imprimindo o fatorial de 5
+
+print(math.pow(2, 3)) #imprimindo 2 elevado a 3
+
+print(math.sin(math.pi/2)) #imprimindo o seno de pi/2
+
+print(math.cos(math.pi)) #imprimindo o cosseno de pi
+
+print(math.tan(math.pi/4)) #imprimindo a tangente de pi/4
+
+print(math.log(100, 10)) #imprimindo o logaritmo de 100 na base 10
+
+print(math.log(100)) #imprimindo o logaritmo natural de 100
+
+print(math.degrees(math.pi)) #imprimindo pi em graus
+
+print(math.radians(180)) #imprimindo 180 graus em radianos
+
+print(math.ceil(4.2)) #imprimindo o teto de 4.2
+
+print(math.floor(4.8)) #imprimindo o piso de 4.8
+
+
+#Importando uma função específica do módulo math
+
+from math import sqrt,sin
+
+print(sqrt(16)) #imprimindo a raiz quadrada de 16
+
+print(sin(math.pi/2)) #imprimindo o seno de pi/2
+
+#Importando todas as funções do módulo math
+
+from math import *
+print(sqrt(25)) #imprimindo a raiz quadrada de 25
+
+import math as m #importando o módulo math com um alias
+
+print(m.sqrt(36)) #imprimindo a raiz quadrada de 36
+
+
+
+
+
